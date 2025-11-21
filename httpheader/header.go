f559ff9b1cade63e8ef7ad88d170ee55a6afbcb8
Toan Nguyen <hgiasac@gmail.com>
2025-11-21 18:05:07 +0700

    fix missing comments

--- httpheader/header.go
+++ httpheader/header.go
@@ -45,112 +45,116 @@ const (
 	// UserAgent is the constant of the User-Agent header name.
 	UserAgent = "User-Agent"
 	// TE is the constant of the TE header name.
-	TE                            = "TE"
+	TE = "TE"
 	// Via is the constant of the Via header name.
-	Via                           = "Via"
-	// Via is the constant of the Via header name.
-	Via                           = "Via"
+	Via = "Via"
 	// Warning is the constant of the Warning header name.
-	Warning                       = "Warning"
+	Warning = "Warning"
 	// Cookie is the constant of the Cookie header name.
-	Cookie                        = "Cookie"
+	Cookie = "Cookie"
 	// Origin is the constant of the Origin header name.
-	Origin                        = "Origin"
+	Origin = "Origin"
 	// AcceptDatetime is the constant of the Accept-Datetime header name.
-	AcceptDatetime                = "Accept-Datetime"
+	AcceptDatetime = "Accept-Datetime"
 	// XRequestedWith is the constant of the X-Requested-With header name.
-	XRequestedWith                = "X-Requested-With"
+	XRequestedWith = "X-Requested-With"
 	// AccessControlAllowOrigin is the constant of the Access-Control-Allow-Origin header name.
-	AccessControlAllowOrigin      = "Access-Control-Allow-Origin"
+	AccessControlAllowOrigin = "Access-Control-Allow-Origin"
 	// AccessControlAllowMethods is the constant of the Access-Control-Allow-Methods header name.
-	AccessControlAllowMethods     = "Access-Control-Allow-Methods"
+	AccessControlAllowMethods = "Access-Control-Allow-Methods"
 	// AccessControlAllowHeaders is the constant of the Access-Control-Allow-Headers header name.
-	AccessControlAllowHeaders     = "Access-Control-Allow-Headers"
+	AccessControlAllowHeaders = "Access-Control-Allow-Headers"
 	// AccessControlAllowCredentials is the constant of the Access-Control-Allow-Credentials header name.
 	AccessControlAllowCredentials = "Access-Control-Allow-Credentials"
 	// AccessControlExposeHeaders is the constant of the Access-Control-Expose-Headers header name.
-	AccessControlExposeHeaders    = "Access-Control-Expose-Headers"
+	AccessControlExposeHeaders = "Access-Control-Expose-Headers"
 	// AccessControlMaxAge is the constant of the Access-Control-Max-Age header name.
-	AccessControlMaxAge           = "Access-Control-Max-Age"
+	AccessControlMaxAge = "Access-Control-Max-Age"
 	// AccessControlRequestMethod is the constant of the Access-Control-Request-Method header name.
-	AccessControlRequestMethod    = "Access-Control-Request-Method"
+	AccessControlRequestMethod = "Access-Control-Request-Method"
 	// AccessControlRequestHeaders is the constant of the Access-Control-Request-Headers header name.
-	AccessControlRequestHeaders   = "Access-Control-Request-Headers"
+	AccessControlRequestHeaders = "Access-Control-Request-Headers"
 	// AcceptPatch is the constant of the Accept-Patch header name.
-	AcceptPatch                   = "Accept-Patch"
+	AcceptPatch = "Accept-Patch"
 	// AcceptRanges is the constant of the Accept-Ranges header name.
-	AcceptRanges                  = "Accept-Ranges"
+	AcceptRanges = "Accept-Ranges"
 	// Allow is the constant of the Allow header name.
-	Allow                         = "Allow"
+	Allow = "Allow"
 	// ContentEncoding is the constant of the Content-Encoding header name.
-	ContentEncoding               = "Content-Encoding"
+	ContentEncoding = "Content-Encoding"
 	// ContentLanguage is the constant of the Content-Language header name.
-	ContentLanguage               = "Content-Language"
+	ContentLanguage = "Content-Language"
 	// ContentLocation is the constant of the Content-Location header name.
-	ContentLocation               = "Content-Location"
+	ContentLocation = "Content-Location"
 	// ContentDisposition is the constant of the Content-Disposition header name.
-	ContentDisposition            = "Content-Disposition"
+	ContentDisposition = "Content-Disposition"
 	// ContentRange is the constant of the Content-Range header name.
-	ContentRange                  = "Content-Range"
+	ContentRange = "Content-Range"
 	// ETag is the constant of the ETag header name.
-	ETag                          = "ETag"
+	ETag = "ETag"
 	// Expires is the constant of the Expires header name.
-	Expires                       = "Expires"
+	Expires = "Expires"
 	// LastModified is the constant of the Last-Modified header name.
-	LastModified                  = "Last-Modified"
+	LastModified = "Last-Modified"
 	// Link is the constant of the Link header name.
-	Link                          = "Link"
+	Link = "Link"
 	// Location is the constant of the Location header name.
-	Location                      = "Location"
+	Location = "Location"
 	// P3P is the constant of the P3P header name.
-	P3P                           = "P3P"
+	P3P = "P3P"
 	// ProxyAuthenticate is the constant of the Proxy-Authenticate header name.
-	ProxyAuthenticate             = "Proxy-Authenticate"
+	ProxyAuthenticate = "Proxy-Authenticate"
 	// RetryAfter is the constant of the Retry-After header name.
-	RetryAfter                    = "Retry-After"
+	RetryAfter = "Retry-After"
 	// Server is the constant of the Server header name.
-	Server                        = "Server"
+	Server = "Server"
 	// SetCookie is the constant of the Set-Cookie header name.
-	SetCookie                     = "Set-Cookie"
+	SetCookie = "Set-Cookie"
 	// StrictTransportSecurity is the constant of the Strict-Transport-Security header name.
-	StrictTransportSecurity       = "Strict-Transport-Security"
+	StrictTransportSecurity = "Strict-Transport-Security"
 	// TransferEncoding is the constant of the Transfer-Encoding header name.
-	TransferEncoding              = "Transfer-Encoding"
+	TransferEncoding = "Transfer-Encoding"
 	// Upgrade is the constant of the Upgrade header name.
-	Upgrade                       = "Upgrade"
+	Upgrade = "Upgrade"
 	// Vary is the constant of the Vary header name.
-	Vary                          = "Vary"
+	Vary = "Vary"
 	// WWWAuthenticate is the constant of the WWW-Authenticate header name.
-	WWWAuthenticate               = "WWW-Authenticate"
+	WWWAuthenticate = "WWW-Authenticate"
 
 	// XFrameOptions is the constant of the X-Frame-Options header name.
-	XFrameOptions          = "X-Frame-Options"
+	XFrameOptions = "X-Frame-Options"
 	// XXSSProtection is the constant of the X-XSS-Protection header name.
-	XXSSProtection         = "X-XSS-Protection"
+	XXSSProtection = "X-XSS-Protection"
 	// ContentSecurityPolicy is the constant of the Content-Security-Policy header name.
-	ContentSecurityPolicy  = "Content-Security-Policy"
+	ContentSecurityPolicy = "Content-Security-Policy"
 	// XContentSecurityPolicy is the constant of the X-Content-Security-Policy header name.
 	XContentSecurityPolicy = "X-Content-Security-Policy"
 	// XWebKitCSP is the constant of the X-WebKit-CSP header name.
-	XWebKitCSP             = "X-WebKit-CSP"
+	XWebKitCSP = "X-WebKit-CSP"
 	// XContentTypeOptions is the constant of the X-Content-Type-Options header name.
-	XContentTypeOptions    = "X-Content-Type-Options"
+	XContentTypeOptions = "X-Content-Type-Options"
 	// XPoweredBy is the constant of the X-Powered-By header name.
-	XPoweredBy             = "X-Powered-By"
+	XPoweredBy = "X-Powered-By"
 	// XUACompatible is the constant of the X-UA-Compatible header name.
-	XUACompatible          = "X-UA-Compatible"
+	XUACompatible = "X-UA-Compatible"
 	// XForwardedProto is the constant of the X-Forwarded-Proto header
 	// that is used to identify the protocol (HTTP or HTTPS) that a visitor used to connect to the proxy server.
-	XForwardedProto     = "X-Forwarded-Proto"
+	XForwardedProto = "X-Forwarded-Proto"
+	// XHTTPMethodOverride is the constant of the X-HTTP-Method-Override header.
 	XHTTPMethodOverride = "X-HTTP-Method-Override"
 	// XForwardedFor is the constant of the X-Forwarded-For header
 	// that maintains proxy server and original visitor IP addresses.
-	XForwardedFor       = "X-Forwarded-For"
-	XRealIP             = "X-Real-IP"
-	XCSRFToken          = "X-CSRF-Token" //nolint:gosec
-	XRatelimitLimit     = "X-Ratelimit-Limit"
+	XForwardedFor = "X-Forwarded-For"
+	// XRealIP is the constant of the X-Real-IP header.
+	XRealIP = "X-Real-IP"
+	// XCSRFToken is the constant of the X-CSRF-Token header.
+	XCSRFToken = "X-CSRF-Token" //nolint:gosec
+	// XRatelimitLimit is the constant of the X-Ratelimit-Limit header.
+	XRatelimitLimit = "X-Ratelimit-Limit"
+	// XRatelimitRemaining is the constant of the X-Ratelimit-Remaining header.
 	XRatelimitRemaining = "X-Ratelimit-Remaining"
-	XRatelimitReset     = "X-Ratelimit-Reset"
+	// XRatelimitReset is the constant of the X-Ratelimit-Reset header.
+	XRatelimitReset = "X-Ratelimit-Reset"
 
 	// Cloudflare headers.
 
