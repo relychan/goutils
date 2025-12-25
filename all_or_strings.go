45402f6b16ffaa16c29d9ba5e91bd9f5962d517b
Toan Nguyen <hgiasac@gmail.com>
2025-12-25 21:15:54 +0700

    add AllOrListWildcardString

--- all_or_strings.go
+++ all_or_strings.go
@@ -3,6 +3,7 @@ package goutils
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"go.yaml.in/yaml/v4"
 )
@@ -118,4 +119,227 @@ func (j AllOrListString) String() string {
 	}
 
 	return fmt.Sprintf("%v", j.list)
+}
+
+// Wildcard represents a string with a wildcard pattern.
+type Wildcard struct {
+	prefix string
+	suffix string
+}
+
+// NewWildcard creates a new Wildcard instance from string.
+func NewWildcard(input string) (Wildcard, bool) {
+	if input == "" {
+		return Wildcard{}, false
+	}
+
+	before, after, found := strings.Cut(input, "*")
+	result := Wildcard{
+		prefix: before,
+		suffix: after,
+	}
+
+	if found {
+		result.suffix = strings.TrimPrefix(after, "*")
+	}
+
+	return result, found
+}
+
+// IsZero returns true if the current instance is in its zero state (neither all nor list is set).
+func (w Wildcard) IsZero() bool {
+	return w.prefix == "" && w.suffix == ""
+}
+
+// Equal checks if the target value is equal.
+func (w Wildcard) Equal(target Wildcard) bool {
+	return w.prefix == target.prefix &&
+		w.suffix == target.suffix
+}
+
+// Match checks if the input string matches the wildcard pattern.
+func (w Wildcard) Match(s string) bool {
+	return len(s) >= (len(w.prefix)+len(w.suffix)) &&
+		strings.HasPrefix(s, w.prefix) && strings.HasSuffix(s, w.suffix)
+}
+
+// String implements the fmt.Stringer interface.
+func (w Wildcard) String() string {
+	return w.prefix + "*" + w.suffix
+}
+
+// AllOrListWildcardString is a type that represents either a wildcard ("*") meaning "all items",
+// or a specific list of strings. Accept a star (*) in any string for sub-matching patterns.
+type AllOrListWildcardString struct { //nolint:recvcheck
+	AllOrListString
+
+	// List of wildcard items. Ignored if all is true.
+	wildcards []Wildcard
+}
+
+// IsZero returns true if the current instance is in its zero state (neither all nor list is set).
+func (j AllOrListWildcardString) IsZero() bool {
+	return j.AllOrListString.IsZero() &&
+		len(j.wildcards) == 0
+}
+
+// Equal checks if the target value is equal.
+func (j AllOrListWildcardString) Equal(target AllOrListWildcardString) bool {
+	return j.AllOrListString.Equal(target.AllOrListString) &&
+		EqualSlice(j.wildcards, target.wildcards, false)
+}
+
+// Wildcards returns the list of wildcards, or nil if IsAll() is true.
+func (j AllOrListWildcardString) Wildcards() []Wildcard {
+	return j.wildcards
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (j *AllOrListWildcardString) UnmarshalJSON(data []byte) error {
+	if string(data) == `"*"` {
+		j.all = true
+		j.list = nil
+		j.wildcards = nil
+
+		return nil
+	}
+
+	var list []string
+
+	err := json.Unmarshal(data, &list)
+	if err != nil {
+		return err
+	}
+
+	j.all = false
+	j.list = nil
+	j.wildcards = nil
+
+	j.parseStrings(list)
+
+	return nil
+}
+
+// MarshalJSON implements json.Marshaler.
+func (j AllOrListWildcardString) MarshalJSON() ([]byte, error) {
+	if j.all {
+		return json.Marshal("*")
+	}
+
+	return json.Marshal(j.toStrings())
+}
+
+// UnmarshalYAML implements custom deserialization for the yaml.Unmarshaler interface.
+// If the YAML value is the string "*", it is treated as a wildcard and sets 'all' to true and 'list' to nil.
+// Otherwise, it expects a list of strings and sets 'list' accordingly, with 'all' set to false.
+func (j *AllOrListWildcardString) UnmarshalYAML(value *yaml.Node) error {
+	if value.Value == "*" {
+		j.all = true
+		j.list = nil
+		j.wildcards = nil
+
+		return nil
+	}
+
+	var list []string
+
+	err := value.Decode(&list)
+	if err != nil {
+		return err
+	}
+
+	j.all = false
+	j.list = nil
+	j.wildcards = nil
+
+	j.parseStrings(list)
+
+	return nil
+}
+
+// MarshalYAML implements the custom behavior for the yaml.Marshaler interface.
+// If the wildcard state is set (all == true), it serializes as the string "*".
+// Otherwise, it serializes the list as a YAML sequence.
+func (j AllOrListWildcardString) MarshalYAML() (any, error) {
+	if j.all {
+		return "*", nil
+	}
+
+	return j.toStrings(), nil
+}
+
+// String implements the custom behavior for the fmt.Stringer interface.
+func (j AllOrListWildcardString) String() string {
+	if j.all {
+		return "*"
+	}
+
+	var sb strings.Builder
+	sb.WriteByte('[')
+
+	for i, str := range j.list {
+		sb.WriteString(str)
+
+		if i < len(j.list)-1 {
+			sb.WriteString(", ")
+		}
+	}
+
+	for _, w := range j.wildcards {
+		sb.WriteString(", ")
+		sb.WriteString(w.String())
+	}
+
+	sb.WriteByte(']')
+
+	return sb.String()
+}
+
+func (j AllOrListWildcardString) toStrings() []string {
+	result := make([]string, 0, len(j.list)+len(j.wildcards))
+	result = append(result, j.list...)
+
+	for _, w := range j.wildcards {
+		result = append(result, w.String())
+	}
+
+	return result
+}
+
+func (j *AllOrListWildcardString) parseStrings(inputs []string) {
+	for _, input := range inputs {
+		j.parseString(input)
+
+		if j.all {
+			return
+		}
+	}
+}
+
+func (j *AllOrListWildcardString) parseString(input string) {
+	if input == "" {
+		j.list = append(j.list, "")
+
+		return
+	}
+
+	// Normalize
+	input = strings.ToLower(input)
+	if input == "*" {
+		// If "*" is present in the list, turn the whole list into a match all
+		j.all = true
+		j.list = nil
+		j.wildcards = nil
+
+		return
+	}
+
+	w, ok := NewWildcard(input)
+	if ok {
+		j.wildcards = append(j.wildcards, w)
+
+		return
+	}
+
+	j.list = append(j.list, input)
 }
