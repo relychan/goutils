be86a0f2ab3f18fa8829fbbd6914356bada658b6
Toan Nguyen <hgiasac@gmail.com>
2026-06-13 11:35:43 +0700

    feat: expose IsURLSchemePrefixHTTP function

--- uri.go
+++ uri.go
@@ -123,7 +123,7 @@ func ParseHTTPURL(s string) (*url.URL, error) {
 		}
 	}
 
-	if !hasHTTPSchemePrefix(input) {
+	if !IsURLSchemePrefixHTTP(input) {
 		return nil, &httperror.ValidationError{
 			Code:   ErrCodeInvalidURI,
 			Detail: "Invalid HTTP URL scheme",
@@ -436,7 +436,8 @@ func parseNormalizedURL(input string) (*url.URL, *httperror.ValidationError) {
 	return parsedURI, nil
 }
 
-func hasHTTPSchemePrefix(input string) bool {
+// IsURLSchemePrefixHTTP checks if the input string has the http(s) scheme.
+func IsURLSchemePrefixHTTP(input string) bool {
 	if len(input) < 7 || !strings.EqualFold(input[:4], "http") {
 		return false
 	}
